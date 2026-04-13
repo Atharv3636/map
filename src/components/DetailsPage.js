eae345e250d7fa97fad365dea8a8c2fb561dfa00
Parasl19 <paraslalasure@gmail.com>
2026-04-13 21:02:41 +0530

    sliceupdate

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -10,7 +10,7 @@ export default function DetailsPage({ statesData }) {
 
   // ✅ GETTING URL PARAMS and NAVIGATE FUNCTION
   const { stateName } = useParams();
-  const navigate = useNavigate();
+
 
 
 
